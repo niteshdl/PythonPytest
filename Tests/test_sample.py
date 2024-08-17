dc5786bda7bd78a398dce18e87dc552de3d1aaab
Nitesh <niteshlachake@gmail.com>
2024-08-17 22:28:13 +0530

    Added sample tests

---
+++ Tests/test_sample.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+def test_sample01():
+    print('sample1')
+    pass
+
+
+def test_sample02():
+    print('sample2')
+    pass
+
+
+def test_sample03():
+    print('sample3')
+    pass
